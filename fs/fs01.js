58a6606f6921c3a70462f09cf790e0f850688db7
wangxiaozhan <w2457307263@outlook.com>
2019-06-29 18:34:27 +0800

    fs.rmdir(path,err)

--- fs/fs01.js
+++ fs/fs01.js
@@ -79,3 +79,10 @@ fs.rename('./fs/1.html', './fs/html/1.html', (err) => {
     if (err) console.log(err)
     console.log('重命名成功!')
 })
+
+/* 8. fs.rmdir() 删除目录 */
+fs.rmdir('html5', (err) => {
+        if (err) console.log('删除失败！')
+        console.log('删除成功！')
+    })
+    /* 9. fs.unlink() 删除文件 */
