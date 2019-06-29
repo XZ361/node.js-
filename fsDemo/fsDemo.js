4a5ca5771082bbf0f047e1f32997d6a37f7a8c49
wangxiaozhan <w2457307263@outlook.com>
2019-06-29 19:07:29 +0800

    fs.readdir(path,data)--Demo

--- fsDemo/fsDemo.js
+++ fsDemo/fsDemo.js
@@ -6,8 +6,21 @@ var fs = require('fs')
         （2）找出某个目录下的所有目录，并打印出来
 */
 
-fs.stat('./fsDemo/upload', (err, stats) => {
+// fs.stat('./fsDemo/upload', (err, stats) => {
+//     if (err) return err
+//     console.log(stats)
+//     console.log(stats.isDirectory())
+// })
+// fs.writeFile('./fsDemo/upload/1.html', 'hello,node.js', (err) => {
+//     if (err) return err
+//     console.log('写入成功！')
+// })
+
+// fs.appendFile('./fsDemo/upload/1.css', '*{margin:0}', (err) => {
+//     if (err) return err
+//     console.log('追加成功！')
+// })
+fs.readdir('./fsDemo/upload', (err, data) => {
     if (err) return err
-    console.log(stats)
-    console.log(stats.isDirectory())
+    console.log('读取目录成功！' + data)
 })
