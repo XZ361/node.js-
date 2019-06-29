b86725d09ff18bd595dbe12d6ce43dd10876840e
wangxiaozhan <w2457307263@outlook.com>
2019-06-29 17:43:12 +0800

    fs.writeFile

---
+++ fs/fs01.js
@@ -0,0 +1,45 @@
+// fs.stat 用来检测是文件还是目录
+
+// var fs = require('fs')
+
+// fs.stat('./html/1.html', (error, stats) => {
+//     // 参数stats代表文件的信息
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(stats)
+//             // 是个文件
+//         console.log("文件：" + stats.isFile())
+//             // 是个目录
+//         console.log("目录：" + stats.isDirectory())
+//     }
+// })
+
+// 2. fs.mkdir 创建目录
+
+// path  将创建的目录的路径
+// mode  目录的权限（读写权限）
+// callback  回调 传递异常err参数
+
+// var fs = require('fs')
+
+// fs.mkdir('html5', (err) => {
+//     if (err) console.log(err)
+
+//     console.log('创建目录成功！')
+// })
+
+// 3. fs.writeFile 写入文件
+// 写入文件的参数：
+// path:要写入文件的路径
+// data:要写入的数据
+// options:设置的参数
+// callback:回调 传回异常参数（err)
+
+var fs = require('fs')
+
+fs.writeFile('index1.txt', 'hello, node.js ,你好!', (err) => {
+    if (err) console.log(err);
+    console.log('写入文件成功！')
+
+})
